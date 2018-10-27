543f00721f1aea8359d8514cf7cad63456dbf8ad
endruu <bibok.andor@gmail.com>
2018-10-27 17:06:00 +0200

    Experimenting

--- main.cpp
+++ main.cpp
@@ -3,7 +3,11 @@
 #include "Example.hpp"
 
 #include <vector>
+#include <queue>
 #include <iostream>
+#include <iterator>
+#include <deque>
+#include <list>
 
 IConnection& operator>>(IConnection& conn, std::vector<Example>& exs)
 {
@@ -26,6 +30,52 @@ IConnection& operator>>(IConnection& conn, std::vector<Example>& exs)
     return conn;
 }
 
+template<typename T>
+struct Loadable
+{
+
+	T load(StatementExtractor& stmt);
+};
+
+auto& extract(StatementExtractor& ext, Example& ex) { ext >> ex.ExInt >> ex.ExString; return ex; }
+
+const char* queryString(const Example&) { return "SELECT ex_int, ex_text FROM ex_table"; }
+
+template<class OutputIt>
+void load(IConnection& conn, OutputIt first)
+{
+	auto stmt = conn.query("SELECT ex_int, ex_text FROM ex_table");
+
+	while (stmt->readNextRow())
+	{
+		//typename std::iterator_traits<OutputIt>::value_type ex;
+		typename OutputIt::container_type::value_type ex;
+		*stmt >> ex.ExInt >> ex.ExString;
+		*first++ = ex;
+	}
+}
+
+template<class OutputIt>
+void load2(IConnection& conn, OutputIt first)
+{
+	typename OutputIt::container_type::value_type ex;
+
+	auto stmt = conn.query(queryString(ex));
+
+	while (stmt->readNextRow())
+	{
+		*first++ = extract(StatementExtractor(*stmt), ex);
+	}
+}
+
+template<typename CollectionT>
+auto load(IConnection& conn)
+{
+	CollectionT collection;
+	load(conn, std::back_inserter(collection));
+	return collection;
+}
+
 int main()
 {
     // const auto db = SqliteDb("db_path") ;
@@ -33,19 +83,39 @@ int main()
 
     std::cout << "BEGIN!" << std::endl;
 
+
     try
     {
 
     std::unique_ptr<IConnection> conn = std::make_unique<SQLiteConnection>(R"(E:\MyPlayground\Cpp\SimpleOrm\test_db.db)");
 
-    std::vector<Example> rows;
+    std::vector<Example> rows1;
+	
 
-    *conn >> rows;
+    *conn >> rows1;
 
-    for(const auto& ex : rows)
+    for(const auto& ex : rows1)
     {
         std::cout << "Example: " << ex.ExInt  << ' ' << ex.ExString << '\n';
     }
+
+	std::deque<Example> rows2;
+
+	//load(*conn, std::front_inserter(rows2));
+	load2(*conn, std::front_inserter(rows2));
+
+	//decltype(*std::front_inserter(rows2)) a;
+
+	for (const auto& ex : rows2)
+	{
+		std::cout << "Example2: " << ex.ExInt << ' ' << ex.ExString << '\n';
+	}
+
+	auto rows3 = load<std::list<Example>>(*conn);
+	for (const auto& ex : rows3)
+	{
+		std::cout << "Example3: " << ex.ExInt << ' ' << ex.ExString << '\n';
+	}
 
     }
     catch(const std::exception& e)
